d9b8c428ae354b53966af86a92c11677f0714b34
Salim Ali <75537889+Salim-Ali-94@users.noreply.github.com>
2023-02-17 22:56:58 +0200

    added error handling

--- Cookie Clicker Bot/utility.py
+++ Cookie Clicker Bot/utility.py
@@ -53,9 +53,10 @@ def loadCheckpoint(browser, shop, opened = True):
 		if f"CC_{'_'.join(shop.split())}_save_file.txt" in os.listdir(os.getcwd()):
 
 			with open(f"CC_{'_'.join(shop.split())}_save_file.txt", "r") as file: previous = file.readlines()
+			WebDriverWait(browser, 30).until(EC.visibility_of_element_located((By.ID, "prefsButton")))
 			options = browser.find_element_by_id("prefsButton")
 			browser.execute_script("arguments[0].click();", options)
-			WebDriverWait(browser, timeout = 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a.option.smallFancyButton")))
+			WebDriverWait(browser, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a.option.smallFancyButton")))
 			load = browser.find_element_by_link_text("Import save")
 			browser.execute_script("arguments[0].click();", load)
 			save_file = browser.find_element_by_id("textareaPrompt")
@@ -67,6 +68,12 @@ def loadCheckpoint(browser, shop, opened = True):
 
 				volume = browser.find_element_by_id("volumeSlider")
 				browser.execute_script("arguments[0].setAttribute('value', 0)", volume)
+				browser.execute_script("arguments[0].dispatchEvent(new Event('change'))", volume)
+
+			else:
+
+				volume = browser.find_element_by_id("volumeSlider")
+				browser.execute_script("arguments[0].setAttribute('value', 75)", volume)
 				browser.execute_script("arguments[0].dispatchEvent(new Event('change'))", volume)
 
 			options = browser.find_element_by_id("prefsButton")
@@ -93,20 +100,40 @@ def getStats(browser):
 	record = {}
 	stats = browser.find_element_by_id("statsButton")
 	browser.execute_script("arguments[0].click();", stats)
-	WebDriverWait(browser, timeout = 30).until(EC.visibility_of_element_located((By.ID, "statsGeneral")))
+	WebDriverWait(browser, 30).until(EC.visibility_of_element_located((By.ID, "statsGeneral")))
 	section = browser.find_element_by_id("statsGeneral")
 	statistics = section.find_elements_by_css_selector(".listing")
-	record[statistics[0].text.split(":", 1)[0]] = statistics[0].find_element_by_css_selector(".price.plain").text.lstrip().rstrip()
-	record[statistics[1].text.split(":", 1)[0]] = statistics[1].find_element_by_css_selector(".price.plain").text.lstrip().rstrip()
-	record[statistics[2].text.split(":", 1)[0]] = statistics[2].find_element_by_css_selector(".price.plain").text.lstrip().rstrip()
-	record[statistics[3].text.split(":", 1)[0]] = " ".join(statistics[3].text.split(":", 1)[1:]).lstrip().rstrip()
-	record[statistics[4].text.split(":", 1)[0]] = " ".join(statistics[4].text.split(":", 1)[1:]).lstrip().rstrip()
-	record[statistics[5].text.split(":", 1)[0]] = " ".join(statistics[5].text.split(":", 1)[1:]).lstrip().rstrip()
-	record[statistics[6].text.split(":", 1)[0]] = " ".join(statistics[6].text.split(":", 1)[1:]).lstrip().rstrip()
-	record[statistics[7].text.split(":", 1)[0]] = " ".join(statistics[7].text.split(":", 1)[1:]).lstrip().rstrip()
-	record[statistics[8].text.split(":", 1)[0]] = " ".join(statistics[8].text.split(":", 1)[1:]).lstrip().rstrip()
-	record[statistics[9].text.split(":", 1)[0]] = " ".join(statistics[9].text.split(":", 1)[1:]).lstrip().rstrip()
-	record[statistics[10].text.split(":", 1)[0]] = " ".join(statistics[10].text.split(":", 1)[1:]).lstrip().rstrip()
+
+	try:
+
+		record[statistics[0].text.split(":", 1)[0]] = statistics[0].find_element_by_css_selector(".price.plain").text.lstrip().rstrip()
+		record[statistics[1].text.split(":", 1)[0]] = statistics[1].find_element_by_css_selector(".price.plain").text.lstrip().rstrip()
+		record[statistics[2].text.split(":", 1)[0]] = statistics[2].find_element_by_css_selector(".price.plain").text.lstrip().rstrip()
+		record[statistics[3].text.split(":", 1)[0]] = " ".join(statistics[3].text.split(":", 1)[1:]).lstrip().rstrip()
+		record[statistics[4].text.split(":", 1)[0]] = " ".join(statistics[4].text.split(":", 1)[1:]).lstrip().rstrip()
+		record[statistics[5].text.split(":", 1)[0]] = " ".join(statistics[5].text.split(":", 1)[1:]).lstrip().rstrip()
+		record[statistics[6].text.split(":", 1)[0]] = " ".join(statistics[6].text.split(":", 1)[1:]).lstrip().rstrip()
+		record[statistics[7].text.split(":", 1)[0]] = " ".join(statistics[7].text.split(":", 1)[1:]).lstrip().rstrip()
+		record[statistics[8].text.split(":", 1)[0]] = " ".join(statistics[8].text.split(":", 1)[1:]).lstrip().rstrip()
+		record[statistics[9].text.split(":", 1)[0]] = " ".join(statistics[9].text.split(":", 1)[1:]).lstrip().rstrip()
+		record[statistics[10].text.split(":", 1)[0]] = " ".join(statistics[10].text.split(":", 1)[1:]).lstrip().rstrip()
+
+	except:
+
+		section = browser.find_element_by_id("statsGeneral")
+		statistics = section.find_elements_by_css_selector(".listing")
+		if statistics[0].text.split(":", 1)[0] in record: record[statistics[0].text.split(":", 1)[0]] = statistics[0].find_element_by_css_selector(".price.plain").text.lstrip().rstrip()
+		if statistics[1].text.split(":", 1)[0] in record: record[statistics[1].text.split(":", 1)[0]] = statistics[1].find_element_by_css_selector(".price.plain").text.lstrip().rstrip()
+		if statistics[2].text.split(":", 1)[0] in record: record[statistics[2].text.split(":", 1)[0]] = statistics[2].find_element_by_css_selector(".price.plain").text.lstrip().rstrip()
+		if statistics[3].text.split(":", 1)[0] in record: record[statistics[3].text.split(":", 1)[0]] = " ".join(statistics[3].text.split(":", 1)[1:]).lstrip().rstrip()
+		if statistics[4].text.split(":", 1)[0] in record: record[statistics[4].text.split(":", 1)[0]] = " ".join(statistics[4].text.split(":", 1)[1:]).lstrip().rstrip()
+		if statistics[5].text.split(":", 1)[0] in record: record[statistics[5].text.split(":", 1)[0]] = " ".join(statistics[5].text.split(":", 1)[1:]).lstrip().rstrip()
+		if statistics[6].text.split(":", 1)[0] in record: record[statistics[6].text.split(":", 1)[0]] = " ".join(statistics[6].text.split(":", 1)[1:]).lstrip().rstrip()
+		if statistics[7].text.split(":", 1)[0] in record: record[statistics[7].text.split(":", 1)[0]] = " ".join(statistics[7].text.split(":", 1)[1:]).lstrip().rstrip()
+		if statistics[8].text.split(":", 1)[0] in record: record[statistics[8].text.split(":", 1)[0]] = " ".join(statistics[8].text.split(":", 1)[1:]).lstrip().rstrip()
+		if statistics[9].text.split(":", 1)[0] in record: record[statistics[9].text.split(":", 1)[0]] = " ".join(statistics[9].text.split(":", 1)[1:]).lstrip().rstrip()
+		if statistics[10].text.split(":", 1)[0] in record: record[statistics[10].text.split(":", 1)[0]] = " ".join(statistics[10].text.split(":", 1)[1:]).lstrip().rstrip()
+
 	stats = browser.find_element_by_id("statsButton")
 	browser.execute_script("arguments[0].click();", stats)
 	return browser, record
@@ -117,7 +144,7 @@ def nameBakery(browser, shop, replace = False):
 	root = os.getcwd()
 	if (os.path.isdir("Save Files") == False): os.makedirs("Save Files")
 	os.chdir("Save Files")
-	WebDriverWait(browser, timeout = 30).until(EC.visibility_of_element_located((By.ID, "bakeryName")))
+	WebDriverWait(browser, 30).until(EC.visibility_of_element_located((By.ID, "bakeryName")))
 
 	if (shop == ""):
 
@@ -156,68 +183,88 @@ def nameBakery(browser, shop, replace = False):
 
 def purchaseItem(browser, inventory, strategy, category):
 
-	if ((strategy == "a") or (strategy == "b")):
+	try:
 
-		if (category == "item"):
+		if ((strategy == "a") or (strategy == "b")):
 
-			items = [int(item.text.split()[-1].replace(",", "")) for item in inventory]
-			index = items.index(max(items) if (strategy == "a") else min(items))
-			buy = inventory[index]
+			if (category == "item"):
 
-		else:
+				items = [int(item.text.split()[-1].replace(",", "")) for item in inventory]
+				index = items.index(max(items) if (strategy == "a") else min(items))
+				buy = inventory[index]
 
-			highest, lowest = 0, float("inf")
+			else:
 
-			for element in inventory:
+				highest, lowest = 0, float("inf")
+
+				for element in inventory:
+
+					actions = ActionChains(browser)
+					actions.move_to_element(element).perform()
+					WebDriverWait(browser, 30).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#tooltip div.name")))
+					tool = ' '.join(browser.find_element_by_css_selector("div#tooltip div.name").text.split())
+					price = browser.find_element_by_css_selector("div#tooltip span.price").text.replace(",", "")
+
+					if ((strategy == "a") and (int(price) > highest)):
+
+						buy = element
+						highest = int(price)
+						selection = tool
+						break
+
+					elif ((strategy == "b") and (int(price) < lowest)):
+
+						buy = element
+						lowest = int(price)
+						selection = tool
+						break
+
+			if (category == "item"): buy_text = buy.text.split()[0:-2 if (len(buy.text.split()) >= 3) else -1]
+			else: buy_text = selection.split()
+			browser.execute_script("arguments[0].click();", buy)
+			print(f"You bought the {' '.join(buy_text)} {category}")
+
+			if (category == "item"):
+
+				upgrades = browser.find_elements_by_css_selector(".crate.upgrade.enabled")
+				return browser, upgrades
+
+			elif (category == "upgrade"):
+
+				return browser
+
+		elif (strategy == "c"):
+
+			item = random.randint(0, len(inventory) - 1)
+			buy = inventory[item]
+
+			if (category == "upgrade"):
 
 				actions = ActionChains(browser)
-				actions.move_to_element(element).perform()
-				WebDriverWait(browser, timeout = 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#tooltip div.name")))
-				tool = ' '.join(browser.find_element_by_css_selector("div#tooltip div.name").text.split())
-				price = browser.find_element_by_css_selector("div#tooltip span.price").text.replace(",", "")
+				actions.move_to_element(buy).perform()
+				WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#tooltip div.name")))
+				selection = browser.find_element_by_css_selector("div#tooltip div.name")
 
-				if ((strategy == "a") and (int(price) > highest)):
-
-					buy = element
-					highest = int(price)
-					selection = tool
-					break
-
-				elif ((strategy == "b") and (int(price) < lowest)):
-
-					buy = element
-					lowest = int(price)
-					selection = tool
-					break
-
-		if (category == "item"): buy_text = buy.text.split()[0:-2 if (len(buy.text.split()) >= 3) else -1]
-		else: buy_text = selection.split()
-		browser.execute_script("arguments[0].click();", buy)
-		print(f"You bought the {' '.join(buy_text)} {category}")
-
-		if (category == "item"):
-
-			upgrades = browser.find_elements_by_css_selector(".crate.upgrade.enabled")
-			return browser, upgrades
-
-		elif (category == "upgrade"):
-
+			if (category == "item"): buy_text = buy.text.split()[0:-2 if (len(buy.text.split()) >= 3) else -1]
+			else: buy_text = selection.text.split()
+			browser.execute_script("arguments[0].click();", buy)
+			print(f"You bought the {' '.join(buy_text)} {category}")
 			return browser
 
-	elif (strategy == "c"):
+	except Exception as error:
 
-		item = random.randint(0, len(inventory) - 1)
-		buy = inventory[item]
+		if (((strategy == "a") or (strategy == "b")) and (category == "item")): return browser, []
+		elif (((strategy == "a") or (strategy == "b")) and (category == "upgrade")): return browser
+		elif (strategy == "c"): return browser
 
-		if (category == "upgrade"):
 
-			actions = ActionChains(browser)
-			actions.move_to_element(buy).perform()
-			WebDriverWait(browser, timeout = 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#tooltip div.name")))
-			selection = browser.find_element_by_css_selector("div#tooltip div.name")
+def isNumber(value):
 
-		if (category == "item"): buy_text = buy.text.split()[0:-2 if (len(buy.text.split()) >= 3) else -1]
-		else: buy_text = selection.text.split()
-		browser.execute_script("arguments[0].click();", buy)
-		print(f"You bought the {' '.join(buy_text)} {category}")
-		return browser
+	try: 
+
+		float(value)
+		return True
+	
+	except: 
+
+		return False
